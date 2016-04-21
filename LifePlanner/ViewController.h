2afd58040c6386c9b1948cd799fcf92de982e7a9
Daniel <Daniel@daniels-macbook-pro.local>
2016-04-21 13:49:25 -0700

    Initial Commit

---
+++ LifePlanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LifePlanner
+//
+//  Created by Daniel on 4/21/16.
+//  Copyright © 2016 Daniel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
